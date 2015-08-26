d4deed2caf9eba5128a4e45ffca2647e7396dd89
sherlockHK <576717731@qq.com>
2015-08-26 22:41:24 +0800

    android learn intent

---
+++ app/src/main/java/com/example/kwhu/helloworld/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.example.kwhu.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by kwhu on 8/25/15.
+ */
+public class SecondActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/kwhu/helloworld/ThirdActivity.java
@@ -0,0 +1,16 @@
+package com.example.kwhu.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by kwhu on 8/26/15.
+ */
+public class ThirdActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.thiird_layout);
+    }
+}
