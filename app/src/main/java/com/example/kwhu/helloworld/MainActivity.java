ff6ec9c450bc759a8f75bf73ac1ea85a662552a0
sherlockHK <576717731@qq.com>
2015-09-11 09:26:32 +0800

    listview basic

--- app/src/main/java/com/example/kwhu/helloworld/MainActivity.java
+++ app/src/main/java/com/example/kwhu/helloworld/MainActivity.java
@@ -1,32 +1,48 @@
 package com.example.kwhu.helloworld;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import adapter.FruitAdapter;
+import model.Fruit;
 
 /**
  * Created by kwhu on 8/26/15.
  */
 public class MainActivity extends BaseActivity {
 
-    public static final String TAG = "MainActivity";
-    private Button button;
-    private EditText editText;
-    private ImageView imageView;
+    private List<Fruit> fruitList = new ArrayList<Fruit>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        if (savedInstanceState != null) {
-            String key = savedInstanceState.getString("key");
-            Log.d("MainActivity", key);
-        }
-
         setContentView(R.layout.activity_main);
+
+        initFruits();
+
+        FruitAdapter fruitAdapter = new FruitAdapter(this,R.layout.fruit_item,fruitList);
+
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(fruitAdapter);
     }
+
+    private void initFruits() {
+        Fruit apple = new Fruit("Apple", R.drawable.google_icon); fruitList.add(apple);
+        Fruit banana = new Fruit("Banana", R.drawable.icon); fruitList.add(banana);
+        Fruit orange = new Fruit("Orange", R.drawable.google_icon); fruitList.add(orange);
+        Fruit watermelon = new Fruit("Watermelon", R.drawable.google_icon); fruitList.add(watermelon);
+        Fruit pear = new Fruit("Pear", R.drawable.google_icon); fruitList.add(pear);
+        Fruit grape = new Fruit("Grape", R.drawable.google_icon); fruitList.add(grape);
+        Fruit pineapple = new Fruit("Pineapple", R.drawable.icon); fruitList.add(pineapple);
+        Fruit strawberry = new Fruit("Strawberry", R.drawable.icon); fruitList.add(strawberry);
+        Fruit cherry = new Fruit("Cherry", R.drawable.icon); fruitList.add(cherry);
+        Fruit mango = new Fruit("Mango", R.drawable.icon); fruitList.add(mango);
+    }
+
 
 
 }
---
+++ app/src/main/java/model/Fruit.java
@@ -0,0 +1,22 @@
+package model;
+
+/**
+ * Created by kwhu on 9/10/15.
+ */
+public class Fruit {
+    private String name;
+    private int imageId;
+
+    public Fruit(String name, int imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+}
